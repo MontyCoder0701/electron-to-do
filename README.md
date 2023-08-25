24426ec63efa63223dbaa9b569a0128574dd8603
Soo <soojlee0106@naver.com>
2023-08-25 18:18:08 +0900

    Use local storage

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # electron-to-do
+
+A simple to-do list app built with Electron
+
+## Project setup
+
+``` bash
+npm install
+```
+
+## Run the app
+
+``` bash
+npm run start
+```
+
+## Build and package the app
+
+``` bash
+npx electron-builder build --mac
+```
--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 window.onload = () => {
   const form1 = document.querySelector("#addForm");
-
-  let items = document.getElementById("items");
-  let submit = document.getElementById("submit");
-
+  const items = document.getElementById("items");
+  const submit = document.getElementById("submit");
   let editItem = null;
+
+  // Check if there are items in localStorage and load them
+  const storedItems = JSON.parse(localStorage.getItem("items")) || [];
+  storedItems.forEach((item) => addItemToList(item));
 
   form1.addEventListener("submit", addItem);
   items.addEventListener("click", removeItem);
@@ -12,52 +14,53 @@ window.onload = () => {
 
 function addItem(e) {
   e.preventDefault();
+  const itemInput = document.getElementById("item");
+  const items = document.getElementById("items");
 
   if (submit.value != "Submit") {
-    console.log("Hello");
-
-    editItem.target.parentNode.childNodes[0].data =
-      document.getElementById("item").value;
-
+    editItem.target.parentNode.childNodes[0].data = itemInput.value;
     submit.value = "Submit";
-    document.getElementById("item").value = "";
-
+    itemInput.value = "";
     document.getElementById("lblsuccess").innerHTML =
       "Text edited successfully";
-
     document.getElementById("lblsuccess").style.display = "block";
-
     setTimeout(function () {
       document.getElementById("lblsuccess").style.display = "none";
     }, 3000);
 
+    // Update the edited item in localStorage
+    updateLocalStorage();
     return false;
   }
 
-  let newItem = document.getElementById("item").value;
-  if (newItem.trim() == "" || newItem.trim() == null) return false;
-  else document.getElementById("item").value = "";
+  let newItem = itemInput.value.trim();
+  if (newItem == "") return false;
+  itemInput.value = "";
 
-  let li = document.createElement("li");
+  // Add the new item to the list
+  addItemToList(newItem);
+
+  // Update localStorage with the new item
+  updateLocalStorage();
+}
+
+function addItemToList(newItem) {
+  const li = document.createElement("li");
   li.className = "list-group-item";
 
-  let deleteButton = document.createElement("button");
-
+  const deleteButton = document.createElement("button");
   deleteButton.className = "btn-danger btn btn-sm float-right delete";
-
   deleteButton.appendChild(document.createTextNode("Delete"));
 
-  let editButton = document.createElement("button");
-
+  const editButton = document.createElement("button");
   editButton.className = "btn-success btn btn-sm float-right edit";
-
   editButton.appendChild(document.createTextNode("Edit"));
 
   li.appendChild(document.createTextNode(newItem));
   li.appendChild(deleteButton);
   li.appendChild(editButton);
 
-  items.appendChild(li);
+  document.getElementById("items").appendChild(li);
 }
 
 function removeItem(e) {
@@ -68,12 +71,13 @@ function removeItem(e) {
       items.removeChild(li);
       document.getElementById("lblsuccess").innerHTML =
         "Text deleted successfully";
-
       document.getElementById("lblsuccess").style.display = "block";
-
       setTimeout(function () {
         document.getElementById("lblsuccess").style.display = "none";
       }, 3000);
+
+      // Remove the item from localStorage
+      updateLocalStorage();
     }
   }
   if (e.target.classList.contains("edit")) {
@@ -84,6 +88,9 @@ function removeItem(e) {
   }
 }
 
-function toggleButton(ref, btnID) {
-  document.getElementById(btnID).disabled = false;
+function updateLocalStorage() {
+  const items = Array.from(document.getElementById("items").children).map(
+    (li) => li.firstChild.data
+  );
+  localStorage.setItem("items", JSON.stringify(items));
 }
